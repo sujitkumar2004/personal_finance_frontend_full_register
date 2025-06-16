0ed557f0b8248af8c1f3489fc87735e1b2a3b31d
SUJIT KUMAR <127448852+sujitkumar2004@users.noreply.github.com>
2025-06-17 04:00:59 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
-// src/services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'https://personal-finance-manager-9ja1.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default api;
